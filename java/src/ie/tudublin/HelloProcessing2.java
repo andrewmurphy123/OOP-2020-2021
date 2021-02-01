6daf6b649a3b059a63e58deaee4d7594bd5b722d
andrewmurphy123 <andymurphy98@gmail.com>
2021-02-01 15:21:10 +0000

    Illuminati

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -13,11 +13,24 @@ public class HelloProcessing2 extends PApplet
 	public void setup() {
 		
 	}
+
+	float x = 0;
+	int i;
+	String s;
+	boolean b;
 	
 	public void draw()
-	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+	{
+		background(255, 0, 0);
+
+		point(200, 60);
+		fill(255, 255, 0);
+		ellipse(250, 280, 460, 460);
+		fill(0, 255, 255);
+		triangle(250, 50, 40, 460, 460, 460);
+		fill(255, 255, 255);
+		ellipse(250, 250, 200, 100);
+		fill(0, 0, 0);
+		ellipse(250, 250, 50, 50);
 	}
 }
